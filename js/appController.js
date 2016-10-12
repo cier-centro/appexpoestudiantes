aecd5b4b1b53be688306741d1c53b80be0d9443f
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2016-10-12 16:23:03 -0500

    Se implementan estilos en la interfaz del slider

---
+++ js/appController.js
@@ -0,0 +1,61 @@
+var objData = "";
+var url = 'service/Resources/Base-expo-estudiantes.json';
+
+$(document).ready(function() {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            objData = data;
+        },
+    });
+
+    contentCarousel();
+
+    function contentCarousel() {
+        
+        console.log(objData);
+        
+        var content = "";
+        
+        $.each(objData, function(k, register) {
+            
+            if(k == 0)
+                content += "<div class='item active'>";
+            else
+                content += "<div class='item'>";
+            
+            content += "<div class='row'>";
+            content += "<div class='col-md-5'>";
+            content += "<div class='row'>";
+            content += "<div class='col-md-12 nombre'>";
+            content += "<h1>"+register.institucion+"</h1>";
+            content += "<h2>"+register.localidad+"</h2>";
+            content += "</div>";
+            content += "</div>";
+            content += "<div class='row'>";
+            content += "<div class='col-md-12 fotografia'>";
+            content += "<img src='"+register.pathSelfie+"' class='img-responsive'/>";
+            content += "</div>";
+            content += "</div>";
+            content += "</div>";
+            content += "<div class='col-md-7 pregunta'>";
+            content += "<h3>"+register.question+"</h3>";
+            content += "<h4>"+register.area+"</h4>";
+            content += "<p>"+register.answer+"</p>";
+            content += "</div>";
+            content += "</div>";
+            content += "</div>";
+            
+        });
+
+        showContentCarousel(content);
+
+    }
+
+    function showContentCarousel(content) {
+        $('#myCarousel').find('.carousel-inner').html(content);
+    }
+
+});
